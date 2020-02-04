6145ff4cddfa0168c47bc16c5df63d8d3093ddf8
Tay Zheng Qiang <zhengqiang@goodsane.com>
2020-02-04 10:50:59 +0800

    Define savePicture()

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -12,19 +12,31 @@ export class PhotoService {
   public async addNewToGallery() {
     //Take a photo
     const capturedPhoto = await Camera.getPhoto({
-      resultType: CameraResultType.Uri,
-      source: CameraSource.Camera,
-      quality: 100
+      resultType: CameraResultType.Uri, // file-based data; provides best performance
+      source: CameraSource.Camera, // automatically take a new photo with the camera
+      quality: 100 // highest quality (0 to 100)
     });
 
-    this.photos.unshift({
-      filepath: "soon...",
-      webviewPath: capturedPhoto.webPath
-    });
+    const savedImageFile = await this.savePicture(capturedPhoto);
+    this.photos.unshift(savedImageFile);
   
   }
 
-  private async savePicture(cameraPhoto: CameraPhoto) { }
+  private async savePicture(cameraPhoto: CameraPhoto) { 
+    // Convert photo to base64 format, required by Filesystem API to save
+     const base64Data = await this.readAsBase64(cameraPhoto);
+
+     //Write the file to the data dictionary
+     const fileName = new Date().getTime() + '.jpeg';
+     await Filesystem.writeFile({
+       path: fileName,
+       data: base64Data,
+       directory: FilesystemDirectory.Data
+     });
+
+     //Get platform-specific photo filepaths
+     return await this.getPhotoFile(cameraPhoto, fileName);
+   }
   
   constructor() { }
 }
