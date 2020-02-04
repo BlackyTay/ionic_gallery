189d9830423f61b237880609cf01e21a522557b3
Tay Zheng Qiang <zhengqiang@goodsane.com>
2020-02-04 14:11:34 +0800

    Import ActionSheetController and inject into Tab2Page.constructor

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { PhotoService } from '../services/photo.service';
-
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -14,6 +14,7 @@ export class Tab2Page {
   ngOnInit() {
     this.photoService.loadSaved();
   }
-  constructor(public photoService: PhotoService) {}
+  constructor(public photoService: PhotoService, 
+              public actionSheetController: ActionSheetController) {}
 
 }
