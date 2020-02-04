5879ecae5c6d3679d8b103a7af8559919d4c663b
Tay Zheng Qiang <zhengqiang@goodsane.com>
2020-02-04 10:57:40 +0800

    Define readAsBase64()

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -38,6 +38,23 @@ export class PhotoService {
      return await this.getPhotoFile(cameraPhoto, fileName);
    }
   
+   private async readAsBase64(cameraPhoto: CameraPhoto) { 
+     //Fetch the photo, read as a blob, then convert to base64 format
+      const response = await fetch(cameraPhoto.webPath!);
+      const bloc = await response.blob();
+
+      return await this.convertBlobToBase64(blob) as string;
+    }
+
+    convertBlobToBase64 = (blob: Blob) => new Promise((resolve,reject) => {
+      const reader = new FileReader;
+      reader.onerror = reject;
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.readAsDataURL(blob);
+    })
+
   constructor() { }
 }
 
